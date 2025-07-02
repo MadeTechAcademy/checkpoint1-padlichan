a81fb0e8add1b799a7d3db6107231a7c5c32b198
padlichan <lili.berenyi@gmail.com>
2025-07-02 14:15:20 +0100

    Test if print_duties prints correct number of lines

--- test_themes.py
+++ test_themes.py
@@ -1,4 +1,5 @@
 from themes import print_duties
+from themes import save_to_html
 
 def test_print_duties_prints_something(capsys):
     print_duties()
@@ -6,3 +7,18 @@ def test_print_duties_prints_something(capsys):
     output = captured.out
     assert output != ""
 
+def test_print_duties_prints_13_lines(capsys):
+    number_of_duties = 13
+    print_duties()
+
+    captured = capsys.readouterr()
+    output = captured.out
+    lines = output.strip().splitlines()
+    non_empty_lines = [line for line in lines if line.strip != 0]
+
+    assert len(non_empty_lines) == number_of_duties
+
+def test_save_to_html_creates_file(tmp_path):
+    output_file = tmp_path/"duties.html"
+    save_to_html(output_file)
+    assert output_file.exists()
